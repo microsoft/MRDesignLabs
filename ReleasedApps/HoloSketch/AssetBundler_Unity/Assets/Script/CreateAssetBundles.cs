c00038aacc4a453fc365446a738717ced05ea265
Patrick Sebring <paseb@microsoft.com>
2017-04-24 12:55:32 -0700

    [~] Added the license header to the script

--- ReleasedApps/HoloSketch/AssetBundler_Unity/Assets/Script/CreateAssetBundles.cs
+++ ReleasedApps/HoloSketch/AssetBundler_Unity/Assets/Script/CreateAssetBundles.cs
@@ -1,4 +1,9 @@
-﻿#if UNITY_EDITOR
+﻿//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+//
+
+#if UNITY_EDITOR
 using UnityEditor;
 
 public class CreateAssetBundles
